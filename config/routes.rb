f11e79c14242f000a619844341f1cb31ee89a503
Aimal Amiri <meaimal@hotmail.com>
2022-08-29 20:14:22 +0430

    Show recipes for the loged in user

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
    root "home#index"
+
+  resources 'recipes', only: %[index]
 end
