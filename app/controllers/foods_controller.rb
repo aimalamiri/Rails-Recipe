787857827c8a811f387c0ee7acaa7311ef27f4fd
ichala <alijandhd@gmail.com>
2022-08-29 18:03:51 +0100

    Fix Linters

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,7 @@
 class FoodsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @OwnedFoods = current_user.foods.all
+    @owned_food = current_user.foods.all
   end
 
   def new
@@ -10,16 +10,16 @@ class FoodsController < ApplicationController
 
   def add_food
     @new_food = current_user.foods.new(food_params)
-    
-    if @new_food.save 
-      redirect_to foods_path, flash: { success: "#{@new_food.name} has been added , with a price of #{@new_food.price}$ per 1 #{@new_food.measurement_unit}" }
+
+    if @new_food.save
+      redirect_to foods_path,
+                  flash: { success: "#{@new_food.name} has been added , with a price of #{@new_food.price}$ per 1 #{@new_food.measurement_unit}" }
     else
       redirect_to foods_new_path, flash: { error: @new_food.errors.full_messages }
     end
   end
 
-
   def food_params
-    params.require(:food).permit(:name,:measurement_unit,:price,:quantity)
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -3,7 +3,7 @@ class Food < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { maximum: 250 }
-  validates :measurement_unit, presence: true, length: { maximum: 250 } 
+  validates :measurement_unit, presence: true, length: { maximum: 250 }
   validates :price, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :quantity, numericality: { only_integer: true }
 end
