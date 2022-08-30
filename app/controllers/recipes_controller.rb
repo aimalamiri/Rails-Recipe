4e34da1faadf8953b86db26630f79bf02bac5f06
Aimal Amiri <meaimal@hotmail.com>
2022-08-30 15:40:13 +0430

    The user can add a new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,9 +7,30 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find params[:id]
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+
+    if @recipe.save
+      redirect_to recipes_path,
+                  flash: { success: "#{@recipe.name} has been successfully created!" }
+    else
+      redirect_to new_recipe_path, flash: { error: @recipes.errors.full_messages }
+    end
+  end
+
   def destroy
     @recipe = Recipe.find params[:id]
     @recipe.destroy!
     redirect_to recipes_path, flash: { success: "#{@recipe.name} has been deleted successfully!" }
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
