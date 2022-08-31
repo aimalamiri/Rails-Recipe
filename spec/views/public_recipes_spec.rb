950354ebd10449c8adf8c357c888fd524a696e94
Aimal Amiri <meaimal@hotmail.com>
2022-08-31 19:00:15 +0430

    Add test for the public_recipes page to have a public recipe

--- spec/views/public_recipes_spec.rb
+++ spec/views/public_recipes_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe 'Public Recipes Page', type: :system do
     User.create(name: 'User 1', email: 'user1@test.com', password: 'password').encrypted_password
     @user = User.last
 
-    Recipe.create(
+    @recipe = Recipe.create(
       name: "Recipe",
       preparation_time: rand(10..60),
       cooking_time: rand(10..60),
       description: "Lorem ipsum dolor sit amit",
-      public: {0 => true, 1 => false}[rand(2)],
+      public: true,
       user_id: @user.id
     )
   end
@@ -18,5 +18,10 @@ RSpec.describe 'Public Recipes Page', type: :system do
   it 'visits public_recipes index page' do
     visit public_recipes_path
     expect(page).to have_content('Here you can find all of the public recipes')
+  end
+
+  it 'visits public_recipes and show the public recipe' do
+    visit public_recipes_path
+    expect(page).to have_content("Recipe##{@recipe.id} by #{@recipe.user.name}")
   end
 end
