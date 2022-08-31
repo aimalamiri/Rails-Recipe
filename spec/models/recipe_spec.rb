4c0f4230e4a077807b36d6fa96baa1bd2b1a895e
Aimal Amiri <meaimal@hotmail.com>
2022-08-31 13:27:38 +0430

    Add test for recipe name to not be over 255 chars

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  before :all do
+  before :each do
     @user = User.create(name: "User 1", email: "user1@test.com", password: 'password').encrypted_password
     @recipe = Recipe.new(name: 'Recipe', preparation_time: 20, cooking_time: 15, description: 'text', public: true, user_id: 1 )
     @recipe.save!
@@ -9,6 +9,11 @@ RSpec.describe Recipe, type: :model do
 
   it 'name should be present' do
     @recipe.name = ''
+    expect(@recipe).to_not be_valid
+  end
+
+  it 'name should not be over 255 characters' do
+    @recipe.name = 'a'*256
     expect(@recipe).to_not be_valid
   end
 end
