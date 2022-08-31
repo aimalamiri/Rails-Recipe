fbfc82e0094231330e453720b8345e6ff150c73d
Aimal Amiri <meaimal@hotmail.com>
2022-08-31 19:39:28 +0430

    Remove unused code from general_shoping_list_controller

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,12 +1,14 @@
 class GeneralShoppingListController < ApplicationController
   before_action :authenticate_user!
-  before_action :authenticate_user!
+
   def index
     @recipes = Recipe.where(user_id: current_user.id)
     @items = RecipeFood.includes(:food).where(recipe_id: @recipes.map(&:id))
     @foods = Food.where(id: @items.map(&:food_id))
     @price = total_price(@items)
   end
+
+  private
 
   def total_price(items)
     price = 0
