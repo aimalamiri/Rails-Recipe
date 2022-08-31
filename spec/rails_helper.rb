84239f2829b917316d2435bafaf73d8806a5ec2e
Aimal Amiri <meaimal@hotmail.com>
2022-08-31 13:37:56 +0430

    Cnfigure chromiom for spec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -63,4 +61,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  Capybara.default_driver = :selenium_chrome
 end
