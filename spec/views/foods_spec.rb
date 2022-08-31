39cfd570f1a9439fff930f5d2c0d5e5d343900ca
Aimal Amiri <meaimal@hotmail.com>
2022-08-31 17:54:55 +0430

    Add test for add new food page

--- spec/views/foods_spec.rb
+++ spec/views/foods_spec.rb
@@ -4,13 +4,22 @@ RSpec.describe 'Foods Page', type: :system do
   before :all do
     User.create(name: 'User 1', email: 'user1@test.com', password: 'password').encrypted_password
     @user = User.last
-    sign_in @user
 
     @food = Food.create(name: 'FOOD1', price: 3, measurement_unit: 'kilos', quantity: 3, user_id: @user.id)
+  end
+
+  before :each do
+    sign_in @user
   end
 
   it 'visits the foods index page' do
     visit foods_path
     expect(page).to have_content(@food.name)
+  end
+
+  it 'visits the foods index page' do
+    visit foods_path
+    find('#add-food').click
+    expect(page).to have_content('Add New Food')
   end
 end
